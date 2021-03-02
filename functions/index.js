0f8ae162a954ca19d1b9338f2dde9d1e3404ddd5
TheJollyFarmer <36710226+TheJollyFarmer@users.noreply.github.com>
2021-03-02 03:13:10 +0000

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 
 exports.handler = async event => {
+  console.log(process.env.RIOT_DATA);
   const url =
     "https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/GrandVice8?api_key=RGAPI-d9f42294-db53-4dca-b4d3-38eaa3b5a2e0";
   const res = axios.get(url);
