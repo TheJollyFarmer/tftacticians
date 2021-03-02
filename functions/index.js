6a1e16baceb1365e58661db6d16212ecb4323124
TheJollyFarmer <thejollyfarmer@icloud.com>
2021-03-02 02:06:40 +0000

    Create index.js

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+
+exports.handler = async event => {
+  const url =
+    "https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/GrandVice8?api_key=RGAPI-d9f42294-db53-4dca-b4d3-38eaa3b5a2e0";
+  const res = axios.get(url);
+
+  return {
+    statusCode: res.status,
+    body: res.data
+  };
+};
